1f46c107c6d1a60961a055da1b44cb4b1be2a0af
Ivan Crnjak <icrnjak@tvz.hr>
2021-03-10 18:06:29 +0100

    added sql ddl and initial data for users

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,4 @@
--- ovdje dodati podatke
-insert into delete_this_table values ( 'x' )
+delete from users;
+
+insert into users (username, password, enabled) values ('admin', '$2a$10$eG28hqAjihXGfSyrNUji9OZEGnMNh66uQUjjIBU0OaaE4Os4u1tom', 1);
+insert into users (username, password, enabled) values ('user', '$2a$10$9P8MUV6cFuWyeys48sQMSe99vu5C1GzwMvsvKvjuvyTCl69p0KsrO', 1);
