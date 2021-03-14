8b2a78e2a7276e9f4e3acb4563f4196f1ee94354
kristijan.zdelarec <kristijan.zdelarecvgmail.com>
2021-03-14 16:26:22 +0100

    Updated Login tests

--- src/test/java/hr/tvz/keepthechange/LoginTest.java
+++ src/test/java/hr/tvz/keepthechange/LoginTest.java
@@ -7,8 +7,13 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /**
@@ -22,15 +27,30 @@ public class LoginTest {
     private MockMvc mockMvc;
 
     /**
-     * Test for performing login. Accessing index page without login will fail.
+     * Test successful login.
      * @throws Exception exception
      */
     @Test
-    public void loginTest() throws Exception {
+    public void loginSuccessTest() throws Exception {
         this.mockMvc
-                .perform(get("/index").with(user("admin").password("adminpass")
-                        .roles("USER", "ADMIN")))
-                .andExpect(status().isOk())
-                .andExpect(view().name("index"));
+                .perform(formLogin().user("admin").password("adminpass"))
+                .andExpect(status().isFound())
+                .andExpect(redirectedUrl("/index"))
+                .andExpect(authenticated().withUsername("admin"));
     }
+
+    /**
+     * Test for failed login
+     * @throws Exception exception
+     */
+    @Test
+    public void loginFailedTest() throws Exception {
+        this.mockMvc
+                .perform(formLogin().user("admin").password("wrongPass"))
+                .andExpect(status().isFound())
+                .andExpect(redirectedUrl("/login?error"))
+                .andExpect(unauthenticated());
+    }
+
+
 }
