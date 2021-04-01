4ca1c81155fb91259693339021e788d31140b151
Ivan Crnjak <icrnjak@tvz.hr>
2021-04-01 21:32:37 +0200

    added AllExpenses page

--- src/main/java/hr/tvz/keepthechange/repository/WalletRepository.java
+++ src/main/java/hr/tvz/keepthechange/repository/WalletRepository.java
@@ -1,11 +1,11 @@
 package hr.tvz.keepthechange.repository;
 
-import hr.tvz.keepthechange.entity.Authority;
 import hr.tvz.keepthechange.entity.Wallet;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -16,4 +16,6 @@ public interface WalletRepository extends CrudRepository<Wallet, Long> {
     ArrayList<Wallet> findByUsername(String username);
 
     Optional<Wallet> findFirstByUsername(String username);
+
+    List<Wallet> findAllByOrderByWalletName();
 }
